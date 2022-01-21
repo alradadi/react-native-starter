4ecb4cf1fc5eadcedb896d6ab29e606b22b24679
Abdullah Alradadi <abdullah@radadi.me>
2022-01-22 01:42:12 +0300

    update eslint configuration
    Signed-off-by: Abdullah Alradadi <abdullah@radadi.me>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
-    },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    '@react-native-community',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
   ],
+  plugins: ['simple-import-sort', 'no-null'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-null/no-null': 'error',
+  },
 };
